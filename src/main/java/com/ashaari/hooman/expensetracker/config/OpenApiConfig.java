f004327608e3f2783833f00f8f94d455d04d44f2
h.ashaari <ashaari.hooman@gmail.com>
2024-08-30 18:56:51 +0330

    Feature/ Added OpenApiConfig

---
+++ src/main/java/com/ashaari/hooman/expensetracker/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.ashaari.hooman.expensetracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${application.title}")
+    private String title;
+    @Value("${application.description}")
+    private String description;
+
+    @Bean
+    public Info info() {
+        return new Info()
+                .title(title)
+                .description(description);
+    }
+
+    @Bean
+    public OpenAPI groupedOpenApi(Info info) {
+        return new OpenAPI().info(info);
+    }
+
+}
