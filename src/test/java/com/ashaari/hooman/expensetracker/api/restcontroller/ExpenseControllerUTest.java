098f912ddd5b2fc9b2e04768f7350873b8669014
h.ashaari <ashaari.hooman@gmail.com>
2024-08-30 21:24:06 +0330

    Feature/ Added ExpenseControllerUTest

---
+++ src/test/java/com/ashaari/hooman/expensetracker/api/restcontroller/ExpenseControllerUTest.java
@@ -0,0 +1,10 @@
+package com.ashaari.hooman.expensetracker.api.restcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ExpenseControllerUTest {
+
+}
