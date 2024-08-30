55a37f886ee4b49ec747c35855b867b9ae62bccb
h.ashaari <ashaari.hooman@gmail.com>
2024-08-30 15:34:30 +0330

    Initialized the project

---
+++ src/test/java/com/ashaari/hooman/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashaari.hooman.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
