a037af32ea454ba809854b4c6508376e002e3572
h.ashaari <ashaari.hooman@gmail.com>
2024-09-01 20:30:53 +0330

    Feature/ Added handleMethodArgumentNotValidException exception handler added "spring-boot-starter-validation" dependency, because validations were not working properly with this version of spring boot

--- src/main/java/com/ashaari/hooman/expensetracker/api/restcontroller/GlobalExceptionHandler.java
+++ src/main/java/com/ashaari/hooman/expensetracker/api/restcontroller/GlobalExceptionHandler.java
@@ -3,21 +3,41 @@ package com.ashaari.hooman.expensetracker.api.restcontroller;
 import com.ashaari.hooman.expensetracker.common.dto.ExceptionObject;
 import com.ashaari.hooman.expensetracker.common.exception.client.ExpenseTrackerClientException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
+    private static final String VALIDATION_EXCEPTION = "ValidationException";
 
     @ExceptionHandler(value = ExpenseTrackerClientException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionObject handleClientException(ExpenseTrackerClientException ex) {
         log.error("Exception happened: {}", ex.toString());
         return new ExceptionObject(ex.getClass().getSimpleName(), ex.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionObject handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Exception happened: {}", ex.toString());
+        FieldError fieldError = ex.getFieldError();
+        if (fieldError != null) {
+            String field = fieldError.getField();
+            return new ExceptionObject(
+                    VALIDATION_EXCEPTION, "Field \"" + field +
+                    "\" has an invalid value of " + fieldError.getRejectedValue());
+        }
+        return new ExceptionObject(VALIDATION_EXCEPTION, "");
     }
 
     @ExceptionHandler(value = Throwable.class)
--- src/main/java/com/ashaari/hooman/expensetracker/common/dto/ExpenseRequestDto.java
+++ src/main/java/com/ashaari/hooman/expensetracker/common/dto/ExpenseRequestDto.java
@@ -1,13 +1,13 @@
 package com.ashaari.hooman.expensetracker.common.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record ExpenseRequestDto(@NotNull BigDecimal amount,
+public record ExpenseRequestDto(@DecimalMin(value = "0.0", inclusive = false) BigDecimal amount,
                                 String description,
                                 @NotEmpty String categoryId,
                                 @Schema(description = "The date on which you spent money. " +
