31a9adc4eee08d33fdba7f38a901312f95d90c03
h.ashaari <ashaari.hooman@gmail.com>
2024-09-01 21:13:25 +0330

    Feature/ Added logMethod in MethodLogger

--- src/main/java/com/ashaari/hooman/expensetracker/aspect/MethodLogger.java
+++ src/main/java/com/ashaari/hooman/expensetracker/aspect/MethodLogger.java
@@ -1,6 +1,8 @@
 package com.ashaari.hooman.expensetracker.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,18 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MethodLogger {
 
-    @Pointcut("execution(@com.ashaari.hooman.expensetracker.aspect.Logged * *(..))")
+    @Pointcut("@within(Logged)")
     public void loggingPointcut() {}
 
+    @Around(value = "loggingPointcut()")
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throw throwable;
+        } finally {
+            log.info(joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        }
+    }
 
 }
