3ae4aa8a984f2624b7c19fdee827696bcc3d72fc
h.ashaari <ashaari.hooman@gmail.com>
2024-09-02 15:01:42 +0330

    Improvement/ added unit test for addCategory API

--- src/test/java/com/ashaari/hooman/expensetracker/api/restcontroller/CategoryControllerUTest.java
+++ src/test/java/com/ashaari/hooman/expensetracker/api/restcontroller/CategoryControllerUTest.java
@@ -1,13 +1,20 @@
 package com.ashaari.hooman.expensetracker.api.restcontroller;
 
+import com.ashaari.hooman.expensetracker.common.dto.AddCategoryRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
 
 import static com.ashaari.hooman.expensetracker.api.restcontroller.util.ControllerTestUtils.EXPENSE_TRACKER_API_V_1;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CategoryController.class)
 class CategoryControllerUTest {
@@ -21,6 +28,17 @@ class CategoryControllerUTest {
     @Test
     @SneakyThrows
     void addCategory_givenNewCategory_savesAndReturnsTheCategory() {
+        AddCategoryRequestDto entertainment =
+                new AddCategoryRequestDto("Entertainment", BigDecimal.valueOf(100));
+
+        String requestBody = objectMapper.writeValueAsString(entertainment);
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post(CATEGORY_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody))
+                .andExpect(status().isCreated())
+                .andReturn();
 
     }
 
