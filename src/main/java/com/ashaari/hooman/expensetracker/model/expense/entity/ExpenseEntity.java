adfbb6ca5a2f472c82b5be745259f80aad1bc1c9
h.ashaari <ashaari.hooman@gmail.com>
2024-09-02 22:42:20 +0330

    Feature/ added ExpenseEntity

---
+++ src/main/java/com/ashaari/hooman/expensetracker/model/expense/entity/ExpenseEntity.java
@@ -0,0 +1,57 @@
+package com.ashaari.hooman.expensetracker.model.expense.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "expense")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpenseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private CategoryEntity category;
+
+    private LocalDateTime spentOn;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpenseEntity that)) return false;
+        return id != null &&
+                Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseEntity{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", spentOn=" + spentOn +
+                '}';
+    }
+
+}
