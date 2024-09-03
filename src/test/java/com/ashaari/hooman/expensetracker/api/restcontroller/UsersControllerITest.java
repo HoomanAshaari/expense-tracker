3d17ebadb8b85ba35ca2f0ef018175523a2b2018
h.ashaari <ashaari.hooman@gmail.com>
2024-09-03 20:25:41 +0330

    Feature/ initiated users controller integration test (UsersControllerITest.java)

---
+++ src/test/java/com/ashaari/hooman/expensetracker/api/restcontroller/UsersControllerITest.java
@@ -0,0 +1,35 @@
+package com.ashaari.hooman.expensetracker.api.restcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+class UsersControllerITest {
+
+    @Container
+    private static final MySQLContainer<?> MYSQL_CONTAINER =
+            new MySQLContainer<>(DockerImageName.parse("mysql:8.4.2"));
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+
+    @Test
+    void test() {
+
+    }
+
+}
