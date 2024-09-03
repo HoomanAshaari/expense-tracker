266169ed7d8e7d86680e463e08718b59f10e7842
h.ashaari <ashaari.hooman@gmail.com>
2024-09-03 22:50:24 +0330

    Improvement/ added SecurityFilterChain bean

--- src/main/java/com/ashaari/hooman/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/ashaari/hooman/expensetracker/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package com.ashaari.hooman.expensetracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.ashaari.hooman.expensetracker.api.restcontroller.util.RestUtils.USERS_API_V_1;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +21,20 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable) // Because we are going to be stateless
+                .authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest
+                        // public endpoints
+                        .requestMatchers(HttpMethod.POST, USERS_API_V_1 + "/sign-up/**").permitAll()
+                        // private endpoints
+                        .anyRequest().authenticated())
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+
 
 }
