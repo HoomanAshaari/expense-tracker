d540e40063faa6f7882e2d3c471d00b444ce8229
h.ashaari <ashaari.hooman@gmail.com>
2024-09-04 22:53:15 +0330

    Improvement/ added getCategoryExpenseStatistics in ExpenseAnalyticsController

--- src/main/java/com/ashaari/hooman/expensetracker/api/restcontroller/ExpenseAnalyticsController.java
+++ src/main/java/com/ashaari/hooman/expensetracker/api/restcontroller/ExpenseAnalyticsController.java
@@ -1,12 +1,18 @@
 package com.ashaari.hooman.expensetracker.api.restcontroller;
 
 import com.ashaari.hooman.expensetracker.business.expense.service.AnalyticsService;
+import com.ashaari.hooman.expensetracker.common.dto.CategoryExpenseStatsDto;
 import com.ashaari.hooman.expensetracker.logger.Logged;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 import static com.ashaari.hooman.expensetracker.api.restcontroller.util.RestUtils.EXPENSE_ANALYTICS_API_V_1;
 
@@ -19,5 +25,16 @@ import static com.ashaari.hooman.expensetracker.api.restcontroller.util.RestUtil
 public class ExpenseAnalyticsController {
 
     private final AnalyticsService analyticsService;
+
+    @GetMapping("/category-expenses")
+    public CategoryExpenseStatsDto getCategoryExpenseStatistics(
+            @RequestParam("dateFrom")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDate dateFrom,
+            @RequestParam("dateTo")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDate dateTo) {
+        return null;
+    }
 
 }
