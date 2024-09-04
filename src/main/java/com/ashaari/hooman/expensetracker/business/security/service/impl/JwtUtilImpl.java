4a31bf7a52c60ebfc45912a16f826f00b4571b22
h.ashaari <ashaari.hooman@gmail.com>
2024-09-04 12:34:09 +0330

    Improvement/ added getSecretKey to be used in getTokenClaims in JwtUtil

--- src/main/java/com/ashaari/hooman/expensetracker/business/security/service/impl/JwtUtilImpl.java
+++ src/main/java/com/ashaari/hooman/expensetracker/business/security/service/impl/JwtUtilImpl.java
@@ -1,11 +1,23 @@
 package com.ashaari.hooman.expensetracker.business.security.service.impl;
 
 import com.ashaari.hooman.expensetracker.business.security.service.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
 @Component
 public class JwtUtilImpl implements JwtUtil {
+
+    @Value("${expense-tracker.jwt.secret}")
+    private String secretKey;
 
     @Override
     public boolean validateToken(String token, UserDetails userDetails) {
@@ -15,12 +27,28 @@ public class JwtUtilImpl implements JwtUtil {
 
     @Override
     public boolean isTokenExpired(String token) {
-        return true;
+        Claims claims = getTokenClaims(token);
+        Date expiration = claims.getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] secretBytes = Base64.getDecoder().decode(secretKey.getBytes(StandardCharsets.UTF_8));
+        return new SecretKeySpec(secretBytes, "HmacSHA256");
     }
 
     @Override
     public String extractUsername(String token) {
         return "";
+    }
+
+    private Claims getTokenClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
     }
 
 }
